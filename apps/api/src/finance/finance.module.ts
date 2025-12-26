2cf00461ab64ecb55ccfe93218b343c658f6517e
suporte.inforcomputer.com <suporte.inforcomputer.com>
2025-12-26 11:25:12 -0300

    fix(api): import NotificationsModule in FinanceModule to resolve DI

--- apps/api/src/finance/finance.module.ts
+++ apps/api/src/finance/finance.module.ts
@@ -3,9 +3,10 @@ import { FinanceService } from './finance.service';
 import { FinanceController } from './finance.controller';
 import { PrismaModule } from '../prisma/prisma.module';
 import { UsersModule } from '../users/users.module';
+import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
-    imports: [PrismaModule],
+    imports: [PrismaModule, NotificationsModule, forwardRef(() => UsersModule)],
     controllers: [FinanceController],
     providers: [FinanceService],
     exports: [FinanceService], // Export for TicketsModule
