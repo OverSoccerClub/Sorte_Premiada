e62566a9721df7c6c65e54c5b522443c0930d94d
OverSoccerClub <suporte.inforcomputer@gmail.com>
2026-02-22 12:38:47 -0300

    chore: Bump version to v1.5.58 (Build 277) and fix logo padding

--- apps/mobile/constants/AppConfig.ts
+++ apps/mobile/constants/AppConfig.ts
@@ -1,5 +1,5 @@
 export const AppConfig = {
-    version: "1.5.57",
+    version: "1.5.58",
     name: "A Perseverança",
     api: {
         // DEV: Use localhost and run 'adb reverse tcp:3333 tcp:3333'
